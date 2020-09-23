d20cbab40f1cbf78075ea835b7726b1a79bb0f32
abdulkadirCildir <a.cildir89@gmail.com>
2020-09-23 18:37:45 +0200

    feat: add index.js and modifie style.css

---
+++ index.js
@@ -0,0 +1,37 @@
+let number = Math.floor(Math.random() * 101);
+let counter = 0;
+
+document.getElementById("check").addEventListener(
+    "click",
+    function() {
+        let num = document.getElementById("nr_gamer").value;
+        counter++;
+        document.getElementById("count").innerHTML= ("This is your " + counter + ". try.");
+
+        if (num == number) {
+            alert("Congratulation! You are the winner! This is your " + counter + ". try.");
+            document.getElementById("result").innerHTML = ("You are right!")
+            
+            let new_game = confirm("Do you wanna play again??");
+            if (new_game == true){
+                number = Math.floor(Math.random() * 101);
+                counter = 0;
+                document.getElementById("result").innerHTML = ("");
+                document.getElementById("count").innerHTML= ("");
+            } else {
+                document.getElementById("count").innerHTML= ("");
+                document.getElementById("result").innerHTML = ("");
+                alert("Thanks for game!!!")
+            }
+
+        } else if (num > number) {
+            // counter ++;
+            document.getElementById("result").innerHTML = ("A lower number please! ")
+        } else {
+            // counter++;
+            document.getElementById("result").innerHTML = ("A higher number please! ")
+        }
+        document.getElementById("nr_gamer").value='';   // Oyun bittikten sonra input icini sifirliyor..
+        document.getElementById("nr_gamer").focus();   // input u focusluyor..
+    }
+)
